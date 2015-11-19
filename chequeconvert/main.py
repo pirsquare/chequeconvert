39d021027fbcd9bb272157dc94a984ef92e86438
Ryan Liao <pirsquare.ryan@gmail.com>
2015-11-20 06:15:15 +0800

    Change argparse arguments name for clarity

--- chequeconvert/main.py
+++ chequeconvert/main.py
@@ -6,7 +6,7 @@ from .base import generate_word
 def main():
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("amt", type=str,
+    parser.add_argument("amount", type=str,
                         help="Amount to convert in string")
     args = parser.parse_args(sys.argv[1:])
-    print generate_word(args.amt)
+    print generate_word(args.amount)
